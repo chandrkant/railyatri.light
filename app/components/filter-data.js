9cd37f08b012d554c0d63e04f5886eaec09ad4ac
Chnadrakant Solanki <chandrkant.solanki@stellingtech.com>
2017-05-17 09:01:27 +0530

    change

--- app/components/filter-data.js
+++ app/components/filter-data.js
@@ -7,6 +7,28 @@ export default Ember.Component.extend({
       $("#ex1").on("slide", function(slideEvt) {
         $("#ex1SliderVal").val(slideEvt.value);
       });
-    })
+      var selector = '.row-body-bt div';
+
+      $(selector).on('click', function(){
+          //$(selector).removeClass('selected');
+          $(this).toggleClass('selected');
+          var arr = [];
+          $(".selected").each(function() {
+            arr.push($(this).data('text'));
+          });
+          $("#row-body-bt").val(arr);
+      });
+      var selector_dt = '.row-body-dt div';
+
+      $(selector_dt).on('click', function(){
+          //$(selector).removeClass('selected');
+          $(this).toggleClass('selected_dt');
+          var arr = [];
+          $(".selected_dt").each(function() {
+            arr.push($(this).data('text'));
+          });
+          $("#row-body-dt").val(arr);
+      });
+    });
   }
 });
