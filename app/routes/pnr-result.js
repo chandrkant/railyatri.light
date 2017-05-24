ee0dd7a13947725ba3fea8f293c61930f18d107e
Chnadrakant Solanki <chandrkant.solanki@stellingtech.com>
2017-05-24 08:28:52 +0530

    pnr

--- app/routes/pnr-result.js
+++ app/routes/pnr-result.js
@@ -2,6 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params){
+    var url = "http://www.indianrail.gov.in/enquiry/PnrEnquiry.html";
+    Ember.$.ajax({
+      url:url,
+      dataType: 'html',
+      success: function(data,status,xhr){
+
+      },error: function(data,status,xhr){
+
+      }
+
+    });
     return params;
   },
   setupController: function (controller, model) {
