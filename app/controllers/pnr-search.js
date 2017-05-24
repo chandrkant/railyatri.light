3bd36f4cf833a93ebd5050eed41ba1d1403fd708
Chandrakant Solanki <chandrkant.solanki@stellingtech.com>
2017-05-24 16:22:40 +0530

    change

--- app/controllers/pnr-search.js
+++ app/controllers/pnr-search.js
@@ -2,10 +2,32 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   title: "PNR Search",
+   myJsonMethod: function(data) {
+   debugger
+  },
   actions: {
      gePnrStatus: function(){
        var pnr_number = Ember.$("#pnr-number").val();
-       this.transitionToRoute('pnr-result',pnr_number);
-     }
-  }
+       var cpt =  Ember.$("#captcha-value").val();
+       // this.transitionToRoute('pnr-result',pnr_number);
+       Ember.$("#captcha-value").val();
+       var url = "http://www.indianrail.gov.in/enquiry/CommonCaptcha?inputCaptcha="+cpt+"&inputPnrNo="+pnr_number+"&inputPage=PNR";
+       console.log(url);
+       Ember.$.ajax({
+	      url:url,
+	      dataType: 'jsonp',
+	      contentType: 'application/json; charset=utf-8',
+		  dataType: 'jsonp',
+		  ajaxSuccess: function(data)
+          {
+           data = JSON.parse(data);
+           console.log("data _" +data);
+          },
+		  jsonpCallback: "myJsonMethod",
+          error: function(data,st,xhr){
+          debugger;
+          } 
+		 });
+		}
+    }
 });
--- app/routes/pnr-search.js
+++ app/routes/pnr-search.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
-
+import config from 'railyatri/config/environment';
 export default Ember.Route.extend({
+	model: function(){
+	},
+	setupController: function (controller, model) {
+		
+	}
 });
