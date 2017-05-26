8fababb10b99bf97e780e9fa65358c0d0acdb5e3
Chnadrakant Solanki <chandrkant.solanki@stellingtech.com>
2017-05-26 08:35:51 +0530

    change

--- app/routes/pnr-result.js
+++ app/routes/pnr-result.js
@@ -2,24 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params){
-    $.ajaxSetup({
-       headers: { 'Host': 'www.indianrail.gov.in','Referer':'http://www.indianrail.gov.in/enquiry/PnrEnquiry.html','Cookie':'JSESSIONID=2wy8qMACCSmbmDPakKo2fZwNKtL8c-NDKESaUOz9.webprs02'}
-    });
-    var api = new RestClient('http://www.indianrail.gov.in/enquiry/captchaDraw.png')
-    api.get().then(function(data){
-       debugger;
-    });
-    // var url = "http://www.indianrail.gov.in/enquiry/captchaDraw.png";
-    // Ember.$.ajax({
-    //   url:url,
-    //   dataType: 'jsonp',
-    //   success: function(data,status,xhr){
-    //    debugger
-    //   },error: function(data,status,xhr){
-    //     debugger;
-    //   }
-
-    // });
     return params;
   },
   setupController: function (controller, model) {
--- app/routes/pnr-search.js
+++ app/routes/pnr-search.js
@@ -1,9 +1,3 @@
 import Ember from 'ember';
-import config from 'railyatri/config/environment';
 export default Ember.Route.extend({
-	model: function(){
-	},
-	setupController: function (controller, model) {
-		
-	}
 });
